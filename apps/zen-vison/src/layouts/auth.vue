d10db46bd88bc864a623f5752415f68e8d2e2beb
Zhao_sj <1640945861@qq.com>
2024-09-02 09:20:33 +0800

    feat: 同步vben改动

--- apps/zen-vison/src/layouts/auth.vue
+++ apps/zen-vison/src/layouts/auth.vue
@@ -1,12 +1,21 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { AuthPageLayout } from '@vben/layouts';
+import { preferences } from '@vben/preferences';
 
 import { $t } from '#/locales';
+
+const appName = computed(() => preferences.app.name);
+const logo = computed(() => preferences.logo.source);
 </script>
 
 <template>
   <AuthPageLayout
+    :app-name="appName"
+    :logo="logo"
     :page-description="$t('zen.core.pageDesc')"
     :page-title="$t('zen.core.pageTitle')"
-  />
+  >
+    <!-- 自定义工具栏 -->
+    <!-- <template #toolbar></template> -->
+  </AuthPageLayout>
 </template>
