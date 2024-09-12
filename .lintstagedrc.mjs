b58a37283e7a754c04289e3e5db4f60a0728ebe8
Zhao_sj <1640945861@qq.com>
2024-09-12 10:09:59 +0800

    chore: reset lintstage config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,19 +1,19 @@
 export default {
-  '**/!public/**/*.{js,jsx,ts,tsx}': [
+  '*.{js,jsx,ts,tsx}': [
     'prettier --cache --ignore-unknown  --write',
     'eslint --cache --fix',
   ],
-  '**/!public/**/*.{scss,less,styl,html,vue,css}': [
+  '*.{scss,less,styl,html,vue,css}': [
     'prettier --cache --ignore-unknown --write',
     'stylelint --fix --allow-empty-input',
   ],
-  '**/!public/**/*.md': ['prettier --cache --ignore-unknown --write'],
-  '**/!public/**/*.vue': [
+  '*.md': ['prettier --cache --ignore-unknown --write'],
+  '*.vue': [
     'prettier --write',
     'eslint --cache --fix',
     'stylelint --fix --allow-empty-input',
   ],
-  '**/!public/**/{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
     'prettier --cache --write--parser json',
   ],
   'package.json': ['prettier --cache --write'],
